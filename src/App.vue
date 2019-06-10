d5cbfa64778f83db028b95500f6434bfa9da1949
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-06-10 12:56:35 +0900

    feat: developer info tooltip, dimigo auth

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app">
     <h1><i class="icon-request" />기상송 투표</h1>
+    <p>학봉관 아침, 여러분의 잠을 깨워 줄 <strong>기상송</strong>을 직접 골라 보세요! 스팸을 막기 위해서 제안한 학생의 실명이 공개됩니다.</p>
     <div class="content">
       <div class="list">
         <div
@@ -25,17 +26,41 @@
     <div class="add">
       <h2 class="add__title">제안하기</h2>
       <div class="add__field">
-        <span class="add__label">노래 제목</span>
+        <span class="add__label">디미고 아이디</span>
         <input type="text" class="add__input" />
       </div>
       <div class="add__field">
-        <span class="add__label">아티스트 이름</span>
+        <span class="add__label">디미고 비밀번호</span>
+        <input type="password" class="add__input" />
+      </div>
+      <div class="add__field">
+        <span class="add__label"><strong>노래 제목</strong></span>
         <input type="text" class="add__input" />
       </div>
-      <button class="add__submit">추가하기</button>
+      <div class="add__field">
+        <span class="add__label"><strong>아티스트 이름</strong></span>
+        <input type="text" class="add__input" />
+      </div>
+      <button
+        class="add__submit"
+      >
+        추가하기
+      </button>
     </div>
     <div class="footer">
-      <i class="icon-dimigo"/> 18HD여준호
+      <a 
+        href="https://github.com/junhoyeo" target="_blank"
+        v-tooltip="'프론트엔드 개발'"
+      >
+        18HD여준호
+      </a>
+      &nbsp;<i class="icon-dimigo"/>&nbsp;
+      <a
+        href="https://github.com/uhmtoto" target="_blank"
+        v-tooltip="'백엔드 개발'"
+      >
+        18HD엄서훈
+      </a>
     </div>
   </div>
 </template>
@@ -48,174 +73,30 @@ export default {
 
     data () {
       return {
-        list: [
-          {
-            'name': '교통정리',
-            'artist': '기리보이',
-            'likes': 5,
-            'hates': 0
-          },
-          {
-            'name': '솔직하게 말해서 나',
-            'artist': '김나영',
-            'likes': 3,
-            'hates': 2
-          }
-        ]
+        list: []
       }
     },
 
     methods: {
       async updateList () {
-        this.$api.get('./api.php')
+        this.$api.get('/songs')
           .then((res) => {
             this.list = res.data.list
           })
+      },
+
+      async likeSong () {
+
+      },
+
+      async hateSong () {
+
       }
     }
 }
 </script>
 
 <style lang="scss">
-@import url('https://cdn.rawgit.com/innks/NanumSquareRound/master/nanumsquareround.min.css');
-@import url('https://cdn.jsdelivr.net/gh/dimigoin/dimigoincon@9e0a536bbf40b5e4aabbbee280acceaea559a4c7/css/dimigoincon.css');
-
-*:focus {
-   outline: none;
-}
-
-html {
-  background-color: #f7f7f7;
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  margin: 0;
-  padding: 0;
-}
-
-h1 {
-  margin-left: 1em;
-  margin-top: 1em;
-  font-family: 'NanumSquareRound', sans-serif;
-}
-
-.content {
-  font-family: 'NanumSquareRound', sans-serif;
-  margin: 0;
-  padding: 0;
-  text-align: center;
-}
-
-.item {
-  margin: auto;
-  text-align: left;
-  font-size: 1.5em;
-  display: table;
-  border-spacing: 0.3em;
-  margin-bottom: 0.5em;
-  width: 90%;
-  border-radius: 18px;
-  border: 0.5px solid rgba(128, 128, 128, 0.2);
-  box-shadow: 15px 19px 32px -18px rgba(21,19,19,.07);
-  padding: 0.5em;
-  
-  &__thumb {
-    display: table-row;
-    color: rgb(172, 172, 172);
-  }
-
-  span {
-    margin-left: 0.5em;
-    cursor: pointer;
-  }
-
-  span.likes:hover {
-    color: rgb(154, 154, 255);
-  }
-
-  span.hates:hover {
-    color: rgb(252, 152, 152);
-  }
-}
-
-.music {
-  &__title {
-    font-weight: 700;
-  }
-
-  &__artist {
-    margin-left: 0.3em !important;
-  }
-}
-
-.add {
-  margin: auto;
-  font-family: 'NanumSquareRound', sans-serif;
-  padding: 0.5em;
-  width: 90%;
-  border-radius: 18px;
-  border: 0.5px solid rgba(128, 128, 128, 0.2);
-  box-shadow: 15px 19px 32px -18px rgba(21,19,19,.07);
-  display: table;
-  border-spacing: 0.5em;
-  padding-left: 1.5em;
-
-  &__title {
-    margin: 0;
-    margin-top: 0.5em;
-  }
-
-  &__field {
-    display: table-row;
-  }
-
-  &__label,
-  &__input {
-    display: table-cell;
-    border-radius: 12px;
-    border: none;
-  }
-
-  &__label {
-    font-size: 1.2em;
-    width: 20%;
-  }
-
-  &__input {
-    font-size: 1em;
-    box-shadow: 15px 19px 32px -18px rgba(21, 19, 19, 0.2);
-    padding: 0.5em;
-    width: 50%;
-  }
-
-  &__submit {
-    width: 100%;
-    display: table-row;
-    font-size: 1.2em;
-    padding: 0.3em;
-    padding-left: 0.5em;
-    padding-right: 0.5em;
-    border-radius: 50px;
-    border: none;
-    box-shadow: 15px 19px 32px -18px rgba(21, 19, 19, 0.2);
-    background-color: #e83c3d;
-    color: white;
-    font-family: 'NanumSquareRound', sans-serif;
-    font-weight: 600;
-  }
-}
-
-.footer {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  bottom: 0;
-  padding: 1em;
-  background-color: #e7e7e7;
-  color: gray;
-}
+@import './style.scss';
+@import './tooltip.css';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import VTooltip from 'v-tooltip'
 import axios from 'axios'
 import App from './App.vue'
 
+Vue.use(VTooltip)
 Vue.prototype.$api = axios
 
 new Vue({
