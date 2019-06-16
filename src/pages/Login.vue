93121009e6b2571dc20d79b1aeb3dbff3b6ca5be
Seohun Uhm <uhmtotow@gmail.com>
2019-06-16 22:43:47 +0900

    refactor: change http error handling

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -80,7 +80,7 @@ export default {
           this.$router.push({ name: 'index' })
         })
         .catch(err => {
-          this.$swal('에러!', err.message, 'error')
+          this.$swal('에러!', err.response.data.error, 'error')
         })
     }
   }
